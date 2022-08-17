81285e7a4f5434bc3268a1650725d56a4e288513
Mayur Saroj <mayursarojdev@gmail.com>
2022-08-17 19:18:45 +0530

    :adhesive_bandage: Prevent ad-blockers from blocking ad cards
    rename classnames to adv

--- src/component/AdvertsContainer/AdvertCard.jsx
+++ src/component/AdvertsContainer/AdvertCard.jsx
@@ -9,7 +9,7 @@ const AdvertCard = ({ ad }) => {
     CTA,
   } = ad;
   return (
-    <div className="advert-card border border-slate-400 rounded shadow">
+    <div className="adv-card border border-slate-400 rounded shadow">
       <div className="img-container overflow-hidden">
         <img
           className="w-full sm:h-64 md:h-72 object-cover sm:hover:scale-110 sm:hover:brightness-90 transition-all duration-500"
@@ -17,21 +17,21 @@ const AdvertCard = ({ ad }) => {
           alt={`poster of advert of company ${companyName}`}
         />
       </div>
-      <div className="advert-body p-4 flex flex-col justify-around">
-        <h4 className="advert-company text-slate-400 uppercase text-sm hover:text-indigo-400">
+      <div className="adv-body p-4 flex flex-col justify-around">
+        <h4 className="adv-company text-slate-400 uppercase text-sm hover:text-indigo-400">
           <a href={companyUrl} target="_blank" rel="noreferrer">
             {companyName}
           </a>
         </h4>
-        <h2 className="advert-headline font-semibold mb-1">{headline}</h2>
-        <h3 className="advert-primary-text font-light text-slate-600 text-sm mb-2">
+        <h2 className="adv-headline font-semibold mb-1">{headline}</h2>
+        <h3 className="adv-primary-text font-light text-slate-600 text-sm mb-2">
           {primaryText}
         </h3>
         <a
           href={companyUrl}
           target="_blank"
           rel="noreferrer"
-          className="advert-cta text-sm text-indigo-500 flex items-center justify-center border px-6 py-2 border-slate-400 rounded transition-all hover:border-slate-600"
+          className="adv-cta text-sm text-indigo-500 flex items-center justify-center border px-6 py-2 border-slate-400 rounded transition-all hover:border-slate-600"
         >
           {CTA}
           <svg
--- src/component/AdvertsContainer/index.js
+++ src/component/AdvertsContainer/index.js
@@ -11,7 +11,7 @@ const AdsContainer = () => {
     <main className="p-4">
       <div className="container mx-auto">
         {adsList.length ? (
-          <div className="adverts-container grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="advs-container grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
             {adsList.map((ad) => {
               return <AdCard key={ad._id} ad={ad} />;
             })}
